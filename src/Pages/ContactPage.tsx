adc3c3eb856d8f2c62c643a69bb613f54b757675
DiegoDeveloperSv <diegocornejo958@gmail.com>
2026-02-05 18:26:10 -0600

    contacts

--- src/Pages/ContactPage.tsx
+++ src/Pages/ContactPage.tsx
@@ -1,4 +1,4 @@
-import { MdiFacebook, MdiInstagram, MdiWhatsapp, MynauiTelephone, TablerMail } from "../components/Icons";
+import { MdiFacebook, MdiWhatsapp, MynauiTelephone, TablerMail } from "../components/Icons";
 
 export function ContactPage() {
     return (
@@ -7,18 +7,15 @@ export function ContactPage() {
                 <h3>Formas de contacto</h3>
                 <ul className="contacts-list">
                     <li className="contact-way">
-                        <a href="https://www.facebook.com/Diego"><MdiFacebook width={24} /> Diego Alexander Cornejo Menjivar</a>
+                        <a href="https://www.facebook.com/profile.php?id=61587177126579"><MdiFacebook width={24} /> Diego Alexander Cornejo Menjivar</a>
                     </li>
                     <li className="contact-way">
-                        <a href="https://www.instagram.com/diexdev"><MdiInstagram width={24} /> diexdev</a>
-                    </li>
-                    <li className="contact-way">
-                        <a href="https://www.whatsapp.com/"><MdiWhatsapp width={24} /> +503 7790-5714</a>
+                        <a href="https://wa.me/50377905714"><MdiWhatsapp width={24} /> +503 7790-5714</a>
                     </li>
                     <li className="contact-way">
                         <a href="tel:+50377905714"><MynauiTelephone width={24} /> +503 7790-5714</a>
                     </li>
-                    <li className="contact-way">    
+                    <li className="contact-way">
                         <a href="mailto:diegocornejo958@gmail.com"><TablerMail width={24} /> diegocornejo958@gmail.com</a>
                     </li>
                 </ul>
